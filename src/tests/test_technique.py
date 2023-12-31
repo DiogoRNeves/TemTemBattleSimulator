a800011405773c7a679e6596f553fe94339b2040
Diogo Neves <neo16pt@gmail.com>
2023-12-31 01:44:20 +0000

    typehinting and mergeddef "test_random_no_type_technique" with "test_random_tech_from_single_type"

--- src/tests/test_technique.py
+++ src/tests/test_technique.py
@@ -1,22 +1,32 @@
+from contextlib import AbstractContextManager, nullcontext as does_not_raise
 import pytest
+
 from hypothesis import given
 from hypothesis.strategies import lists, sampled_from
 
 from src.technique import Technique
 from src.tem_tem_type import TemTemType
 
-@pytest.mark.parametrize("type_to_choose", [t for t in TemTemType if t != TemTemType.NO_TYPE])
-def test_random_tech_from_single_type(type_to_choose: TemTemType) -> None:
-    t = Technique.get_random_technique(type_to_choose)
-    assert t.type == type_to_choose
-
-def test_random_no_type_technique() -> None:
-    with pytest.raises(ValueError):
-        Technique.get_random_technique(TemTemType.NO_TYPE)
+@pytest.mark.parametrize(
+    "type_to_choose, expectation",
+    [
+        (
+            t,
+            does_not_raise() if t != TemTemType.NO_TYPE else pytest.raises(ValueError)
+        ) for t in TemTemType
+    ]
+)
+def test_random_tech_from_single_type(
+    type_to_choose: TemTemType,
+    expectation: AbstractContextManager[BaseException]
+) -> None:
+    with expectation:
+        t = Technique.get_random_technique(type_to_choose)
+        assert t.type == type_to_choose
 
 @given(
     lists(
-        elements= sampled_from(TemTemType),
+        elements=sampled_from(TemTemType),
         min_size=2,
         max_size=len(TemTemType),
         unique=True
