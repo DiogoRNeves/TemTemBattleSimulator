038797a474bca411b8481a25f5c66fda4607fbcd
Diogo Neves <neo16pt@gmail.com>
2024-01-18 00:17:59 +0000

    made battle test use any /all implemented agents

--- src/tests/test_battle.py
+++ src/tests/test_battle.py
@@ -1,5 +1,5 @@
 from hypothesis import event, given, strategies as st
-from src.battle_agent import BattleAgent, RandomBattleAgent
+from src.battle_agent import BattleAgent
 from src.battle_team import Teams
 from src.battle import Battle
 from src.battle_handler import TamerBattleHandler
@@ -7,9 +7,11 @@ from src.battle_state import BattleResult, BattleState
 from src.team import PlaythroughTeam
 
 @given(
-    seed=st.random_module()
+    seed=st.random_module(),
+    orange_agent_class=st.sampled_from(BattleAgent.__subclasses__()),
+    blue_battle_class=st.sampled_from(BattleAgent.__subclasses__())
 )
-def test_random_action_battle_ends(seed):
+def test_ai_agent_battle_ends(seed, orange_agent_class, blue_battle_class):
     event(seed)
     s = BattleState(
         team_orange=PlaythroughTeam.get_random(),
@@ -22,8 +24,8 @@ def test_random_action_battle_ends(seed):
     )
 
     p: dict[Teams, BattleAgent] = {
-        Teams.BLUE: RandomBattleAgent(),
-        Teams.ORANGE: RandomBattleAgent()
+        Teams.BLUE: orange_agent_class(),
+        Teams.ORANGE: blue_battle_class()
     }
 
     # this one throws a lot of NotImplementedException at the moment :-)
